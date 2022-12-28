2600eea001f6ab49208b68b26b905d8516337aa2
kizey <ikizey@icloud.com>
2022-12-29 03:42:35 +0400

    setup base routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
+import Demo from './pages/Demo';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
 
 const App = () => {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path='/'>
+        <Route index element={<Home />} />
+        <Route path='demo' element={<Demo />} />
+        <Route path='login' element={<Login />} />
+        <Route path='profile' element={<Profile />} />
+        <Route path='signup' element={<Signup />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
