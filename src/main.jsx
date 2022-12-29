a1d6ead74f0f12ad188ce39a6a1f9a0fe7bd74ce
kizey <ikizey@icloud.com>
2022-12-29 18:35:36 +0400

    add authContext

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import AuthContextProvider from './store/AuthContext';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/store/AuthContext.jsx
@@ -0,0 +1,24 @@
+import { createContext, useEffect, useState } from 'react';
+import { supabase } from '../supabaseClient';
+
+export const AuthContext = createContext();
+
+const AuthContextProvider = ({ children }) => {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ session }}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthContextProvider;
