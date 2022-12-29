ec8bbaa8a4ff780ec322dcac268e0f353221ddbd
kizey <ikizey@icloud.com>
2022-12-29 18:19:26 +0400

    add route constant

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import { Route, Routes } from 'react-router-dom';
+import { ROUTE } from './pages/Route';
 import Demo from './pages/Demo';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Profile from './pages/Profile';
 import Register from './pages/Register';
+import ResetPassword from './pages/ResetPassword';
+import ResetPasswordFinish from './pages/ResetPasswordFinish';
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/'>
-        <Route index element={<Home />} />
-        <Route path='demo' element={<Demo />} />
-        <Route path='login' element={<Login />} />
-        <Route path='profile' element={<Profile />} />
-        <Route path='signup' element={<Register />} />
-      </Route>
+      <Route path={ROUTE.INDEX} element={<Home />} />
+      <Route path={ROUTE.DEMO} element={<Demo />} />
+      <Route path={ROUTE.LOGIN} element={<Login />} />
+      <Route path={ROUTE.PROFILE} element={<Profile />} />
+      <Route path={ROUTE.REGISTER} element={<Register />} />
+      <Route path={ROUTE.RESET_PASSWORD} element={<ResetPassword />} />
+      <Route path={ROUTE.SET_NEW_PASSWORD} element={<ResetPasswordFinish />} />
     </Routes>
   );
 };
---
+++ src/pages/Route.js
@@ -0,0 +1,9 @@
+export const ROUTE = Object.freeze({
+  INDEX: '/',
+  DEMO: '/demo',
+  LOGIN: '/login',
+  PROFILE: '/profile',
+  REGISTER: '/register',
+  RESET_PASSWORD: '/reset-password',
+  SET_NEW_PASSWORD: '/set-new-password',
+});
