65c3c8db1d79ef5b0ff38d58d0410de3c92f0c25
kizey <ikizey@icloud.com>
2022-12-29 18:35:12 +0400

    setup supabase client

---
+++ src/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const URL = import.meta.env.VITE_SUPABASE_URL;
+const KEY = import.meta.env.VITE_SUPABASE_KEY;
+
+export const supabase = createClient(URL, KEY);
