957d58029aed20ca594c6050141e78b495868cd1
kizey <ikizey@icloud.com>
2022-12-30 01:07:48 +0400

    refactor

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,126 +1,33 @@
-import { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { supabase } from '../supabaseClient';
-import { AuthContext } from '../store/AuthContext';
 import { ROUTE } from './Route';
+import NavBar from '../components/NavBar';
 
 const Home = () => {
-  const [isFetching, setIsFetching] = useState(false);
-
-  const { session } = useContext(AuthContext);
-
-  const logout = async (_) => {
-    try {
-      setIsFetching(true);
-      const { error } = await await supabase.auth.signOut();
-      if (error) {
-        throw new Error(error);
-      }
-    } catch (error) {
-      alert('connection problem');
-    } finally {
-      setIsFetching(false);
-    }
-  };
-
   return (
     <div className='isolate bg-white'>
-      <div className='px-6 pt-6 lg:px-8'>
-        <div>
-          <nav
-            className='flex h-9 items-center justify-between'
-            aria-label='Global'
-          >
-            <div className='flex lg:min-w-0 lg:flex-1' aria-label='Global'>
-              <a href='#' className='-m-1.5 p-1.5'>
-                <span className='sr-only'>Aibacus</span>
-                <img className='h-8' src='grid-outline.svg' alt='' />
-              </a>
-            </div>
-            <div className='flex'></div>
-            <div className='lg:flex lg:min-w-0 lg:flex-1 lg:justify-center lg:gap-x-12'>
-              <a
-                href='generate'
-                className='font-semibold text-gray-900 hover:text-gray-400 mr-2'
-              >
-                Arithmetics
-              </a>
-
-              <a
-                href='generate_m'
-                className='font-semibold text-gray-900 hover:text-gray-400 mr-2 ml-2'
-              >
-                Math
-              </a>
-
-              <a
-                href='generate_w'
-                className='font-semibold text-gray-900 hover:text-gray-400 ml-2'
-              >
-                Writing
-              </a>
-            </div>
-            <div className='lg:flex lg:min-w-0 lg:flex-1 lg:justify-end'>
-              {!session ? (
-                <>
-                  <Link
-                    to={ROUTE.LOGIN}
-                    className='inline-block rounded-lg px-3 py-1.5 text-sm font-semibold leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20'
-                  >
-                    Log in
-                  </Link>
-                  <Link
-                    to={ROUTE.REGISTER}
-                    className='inline-block rounded-lg ml-2 px-3 py-1.5 text-sm font-semibold leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20'
-                  >
-                    Sign up
-                  </Link>
-                </>
-              ) : (
-                <div className='flex items-center'>
-                  <Link to={ROUTE.PROFILE}>{session?.user.email}</Link>
-                  <button
-                    className='inline-block rounded-lg ml-2 px-3 py-1.5 text-sm font-semibold leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20'
-                    onClick={logout}
-                    disabled={isFetching}
-                  >
-                    Log out
-                  </button>
-                </div>
-              )}
-            </div>
-          </nav>
-          <div role='dialog' aria-modal='true'></div>
-        </div>
-      </div>
+      <NavBar />
       <main>
         <div className='relative px-6'>
           <div className='mx-auto max-w-3xl'>
-            <div>
-              <div>
-                <h1 className='text-4xl font-bold tracking-tight sm:text-center sm:text-6xl mt-16 mb-6'>
-                  Experience the power of screen-free education
-                </h1>
-                <img src='main.png' className='mx-auto rounded-xl mb-6 w-1/2' />
-
-                <p className='mt-6 text-lg leading-8 text-gray-600 sm:text-center'>
-                  We help K-12 studens to learn without screens. Instantly
-                  generate assignments and print them for self-study. Analyze
-                  and track student progress. Share your ideas with other
-                  parents.
-                </p>
-                <div className='mt-8 flex gap-x-4 sm:justify-center'>
-                  <Link
-                    to={ROUTE.DEMO}
-                    className='inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold text-white shadow-lg ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700'
-                  >
-                    Demo
-                    <span className='text-indigo-200' aria-hidden='true'>
-                      &rarr;
-                    </span>
-                  </Link>
-                </div>
-              </div>
+            <h1 className='text-lg font-bold tracking-tight sm:text-center sm:text-3xl mt-16 mb-6'>
+              Experience the power of screen-free education
+            </h1>
+            <img src='main.png' className='mx-auto rounded-xl mb-6 w-1/2' />
+            <p className='mt-6 text-lg leading-8 text-gray-600 sm:text-center'>
+              We help K-12 studens to learn without screens. Instantly generate
+              assignments and print them for self-study. Analyze and track
+              student progress. Share your ideas with other parents.
+            </p>
+            <div className='mt-8 flex gap-x-4 sm:justify-center'>
+              <Link
+                to={ROUTE.DEMO}
+                className='px-6 py-2 my-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900'
+              >
+                Demo
+                <span className='ml-1 text-white' aria-hidden='true'>
+                  &rarr;
+                </span>
+              </Link>
             </div>
           </div>
         </div>
