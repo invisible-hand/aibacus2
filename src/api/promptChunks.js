b3b984b5581c9f74be4aada53e147b7ddc6992f3
kizey <ikizey@icloud.com>
2022-12-31 03:56:54 +0400

    add promptChunks set of constants

---
+++ src/api/promptChunks.js
@@ -0,0 +1,73 @@
+const MATH = Object.freeze({
+  name: 'math',
+  skills: {
+    addition: 'addition',
+    subtraction: 'subtraction',
+    multiplication: 'multiplication',
+    division: 'division',
+  },
+});
+
+const READING = Object.freeze({
+  name: 'reading',
+  skills: {
+    addition: '...',
+    subtraction: '...',
+    multiplication: '...',
+    division: '...',
+  },
+});
+
+const SUBJECT = Object.freeze({
+  MATH,
+  READING: 'reading',
+});
+
+export const NUMBER_OF_TASKS = [
+  'single task',
+  'single task',
+  'two tasks',
+  'three tasks',
+  'four tasks',
+  'five tasks',
+  'six tasks',
+  'seven tasks',
+  'eight tasks',
+  'nine tasks',
+  'ten tasks',
+  'eleven tasks',
+  'twelve tasks',
+  'thirteen tasks',
+  'fourteen tasks',
+  'fifteen tasks',
+  'sixteen tasks',
+  'seventeen tasks',
+  'eighteen tasks',
+  'nineteen tasks',
+  'twenty tasks',
+  'twenty-one tasks',
+  'twenty-two tasks',
+  'twenty-three tasks',
+  'twenty-four tasks',
+  'twenty-five tasks',
+  'twenty-six tasks',
+  'twenty-seven tasks',
+  'twenty-eight tasks',
+  'twenty-nine tasks',
+  'thirty tasks',
+];
+
+export const GRADE = [
+  '1st',
+  '2nd',
+  '3rd',
+  '4th',
+  '5th',
+  '6th',
+  '7th',
+  '8th',
+  '9th',
+  '10th',
+  '11th',
+  '12th',
+];
