7a97617b043304027ce5c69c5dd7412c998bcf1b
kizey <ikizey@icloud.com>
2022-12-31 19:42:04 +0400

    fix PDF overwriting.

--- src/pdf/pdfSave.jsx
+++ src/pdf/pdfSave.jsx
@@ -1,9 +1,8 @@
 import { jsPDF } from 'jspdf';
 
-const doc = new jsPDF();
-
 export const pdfSave = (name, grade, assignment) => {
   const lines = assignment.join('\n');
+  const doc = new jsPDF();
   doc.text(`Student: ${name} \nGrade: ${grade}\n\n${lines}`, 15, 15);
   doc.save('arithmetics.pdf');
 };
