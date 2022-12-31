28c34f6f0eff1b9263b0b3f012b0b4f763ec06c0
kizey <ikizey@icloud.com>
2022-12-31 20:06:07 +0400

    add ProtectedRoute

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
-import { ROUTE } from './pages/Route';
+
 import Demo from './pages/Demo';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Math from './pages/Math';
 import Profile from './pages/Profile';
+import ProtectedRoute from './components/ProtectedRoute';
+import { ROUTE } from './pages/Route';
 import Register from './pages/Register';
 import ResetPassword from './pages/ResetPassword';
 import SetNewPassword from './pages/SetNewPassword';
-import Math from './pages/Math';
 // import Layout from './components/layout/Layout';
 
 const App = () => {
@@ -16,11 +18,32 @@ const App = () => {
       <Route path={ROUTE.INDEX} element={<Home />} />
       <Route path={ROUTE.DEMO} element={<Demo />} />
       <Route path={ROUTE.LOGIN} element={<Login />} />
-      <Route path={ROUTE.PROFILE} element={<Profile />} />
+      <Route
+        path={ROUTE.PROFILE}
+        element={
+          <ProtectedRoute>
+            <Profile />
+          </ProtectedRoute>
+        }
+      />
       <Route path={ROUTE.REGISTER} element={<Register />} />
       <Route path={ROUTE.RESET_PASSWORD} element={<ResetPassword />} />
-      <Route path={ROUTE.SET_NEW_PASSWORD} element={<SetNewPassword />} />
-      <Route path={ROUTE.MATH} element={<Math />} />
+      <Route
+        path={ROUTE.SET_NEW_PASSWORD}
+        element={
+          <ProtectedRoute>
+            <SetNewPassword />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path={ROUTE.MATH}
+        element={
+          <ProtectedRoute>
+            <Math />
+          </ProtectedRoute>
+        }
+      />
     </Routes>
   );
 };
---
+++ src/components/ProtectedRoute.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react';
+
+import { AuthContext } from '../store/AuthContext';
+import { Navigate } from 'react-router-dom';
+import { ROUTE } from '../pages/Route';
+
+const ProtectedRoute = ({ children }) => {
+  const { session } = useContext(AuthContext);
+
+  if (session?.user.aud === 'authenticated') {
+    return children;
+  } else {
+    return <Navigate to={ROUTE.LOGIN} />;
+  }
+};
+
+export default ProtectedRoute;
