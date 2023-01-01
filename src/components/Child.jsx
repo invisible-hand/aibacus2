bdb938b4623f4d8138fb038dccbf1b99da4d835f
kizey <ikizey@icloud.com>
2023-01-02 01:34:39 +0400

    reset name and grade after add

--- src/components/Child.jsx
+++ src/components/Child.jsx
@@ -27,7 +27,9 @@ const Child = ({ id, name, grade, onClick, disabled }) => {
         onClick={() => {
           id
             ? onClick(id, nameState, gradeState)
-            : onClick(nameState, gradeState);
+            : (onClick(nameState, gradeState),
+              setNameState(''),
+              setGradeState('1'));
         }}
         disabled={disabled || false}
         className='px-6 py-1 ml-1 my-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900 disabled:bg-blue-200 hover:disabled:bg-blue-200'
