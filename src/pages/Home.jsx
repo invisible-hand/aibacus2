2bc2b0ae3a324c0f7e212573bddee99510194e58
kizey <ikizey@icloud.com>
2023-01-01 20:48:27 +0400

    change styling to specifications

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,28 +3,31 @@ import { ROUTE } from '../constants/Route';
 
 const Home = () => {
   return (
-    <>
-      <h1 className='text-lg font-bold tracking-tight sm:text-center sm:text-3xl mt-16 mb-6'>
-        Experience the power of screen-free education
-      </h1>
+    <div className='flex mx-5 flex-col md:flex-row md:mx-auto gap-2 md:mt-16'>
       <img src='main.png' className='mx-auto rounded-xl mb-6 w-1/2' />
-      <p className='mt-6 text-lg leading-8 text-gray-600 sm:text-center'>
-        We help K-12 studens to learn without screens. Instantly generate
-        assignments and print them for self-study. Analyze and track student
-        progress. Share your ideas with other parents.
-      </p>
-      <div className='mt-8 flex gap-x-4 sm:justify-center'>
-        <Link
-          to={ROUTE.DEMO}
-          className='px-6 py-2 my-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900'
-        >
-          Demo
-          <span className='ml-1 text-white' aria-hidden='true'>
-            &rarr;
-          </span>
-        </Link>
+      <div>
+        <h1 className='text-lg font-bold tracking-tight sm:text-3xl mb-6 mx-auto text-center'>
+          Experience the power of screen-free education
+        </h1>
+
+        <p className='mt-6 text-lg leading-8 text-gray-600 sm:text-center'>
+          We help K-12 studens to learn without screens. Instantly generate
+          assignments and print them for self-study. Analyze and track student
+          progress. Share your ideas with other parents.
+        </p>
+        <div className='mt-8 flex gap-x-4 sm:justify-center'>
+          <Link
+            to={ROUTE.DEMO}
+            className='px-6 py-2 my-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900 mx-auto'
+          >
+            Demo
+            <span className='ml-1 text-white' aria-hidden='true'>
+              &rarr;
+            </span>
+          </Link>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
