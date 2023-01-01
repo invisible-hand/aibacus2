ec050c6c96f1b2128df4739c5025d7b373be11a3
kizey <ikizey@icloud.com>
2023-01-02 01:19:10 +0400

    remove logs

--- src/database/children.js
+++ src/database/children.js
@@ -1,9 +1,6 @@
 import { supabase } from '../supabaseClient';
 
 export const addChildToDB = async (name, grade, user_id) => {
-  console.log(`trying to add child`);
-  console.log(`name: ${name}`);
-  console.log(`grade: ${grade}`);
   const { error } = await supabase.from('children').insert({
     name,
     grade,
