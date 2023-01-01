8077284e0c58eac15cf7e0bd9a535f3f2358cb7e
kizey <ikizey@icloud.com>
2023-01-01 05:42:18 +0400

    use delete function

--- src/components/DeleteAssignment.jsx
+++ src/components/DeleteAssignment.jsx
@@ -1,17 +1,10 @@
-import React from 'react';
-import { supabase } from '../supabaseClient';
+import { deleteAssignment } from '../database/deleteAssignment';
 
 const DeleteAssignment = ({ assignmentId }) => {
-  const deleteAssignment = async (id) => {
+  const handleDelete = async (id) => {
     //TODO! update list after delete
     try {
-      const { error } = await supabase
-        .from('assignments')
-        .delete()
-        .eq('id', id);
-      if (error) {
-        throw new Error(error);
-      }
+      await deleteAssignment(assignmentId);
     } catch (error) {
       alert(error.message);
     }
@@ -20,7 +13,7 @@ const DeleteAssignment = ({ assignmentId }) => {
     <button
       className='px-6 py-1 ml-1 my-4 text-white bg-red-600 rounded-lg hover:bg-red-900 disabled:bg-red-200 hover:disabled:bg-red-200'
       onClick={(_) => {
-        deleteAssignment(assignmentId);
+        handleDelete(assignmentId);
       }}
     >
       Delete
