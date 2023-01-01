bdbdc6575c90f4d4c063457b5981d481205b30de
kizey <ikizey@icloud.com>
2023-01-02 00:36:19 +0400

    add Child component

---
+++ src/components/Child.jsx
@@ -0,0 +1,41 @@
+import GradePicker from './GradePicker';
+import { useState } from 'react';
+
+const Child = ({ id, name, grade, onClick, disabled }) => {
+  const [nameState, setNameState] = useState(name);
+  const [gradeState, setGradeState] = useState(grade);
+
+  return (
+    <>
+      <label htmlFor='name' className='mr-1'>
+        Name
+      </label>
+      <input
+        id='name'
+        name='name'
+        type='text'
+        placeholder='enter name...'
+        value={nameState}
+        onChange={(e) => {
+          setNameState(e.target.value);
+        }}
+        className='px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600'
+      />
+
+      <GradePicker defaultValue={gradeState} onChange={setGradeState} />
+      <button
+        onClick={() => {
+          id
+            ? onClick(id, nameState, gradeState)
+            : onClick(nameState, gradeState);
+        }}
+        disabled={disabled || false}
+        className='px-6 py-1 ml-1 my-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900 disabled:bg-blue-200 hover:disabled:bg-blue-200'
+      >
+        {id ? 'Update' : 'Add'}
+      </button>
+    </>
+  );
+};
+
+export default Child;
