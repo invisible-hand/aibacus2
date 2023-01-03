38ae06d9b916adefbbfd93555817ee8940fbad1c
kizey <ikizey@icloud.com>
2023-01-03 21:35:04 +0400

    remove flex-1

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import { ROUTE } from '../constants/Route';
 
 const Logo = () => {
   return (
-    <div className='flex lg:min-w-0 lg:flex-1' aria-label='Global'>
+    <div className='flex lg:min-w-0 ' aria-label='Global'>
       <Link to={ROUTE.INDEX} className='-m-1.5 p-1.5'>
         <span className='sr-only'>Aibacus</span>
         <img className='h-8' src='grid-outline.svg' alt='' />
