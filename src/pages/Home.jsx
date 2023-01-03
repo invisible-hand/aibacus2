19c2b8edde620eca2d81d970dd8d125fda8ac9eb
kizey <ikizey@icloud.com>
2023-01-03 21:36:54 +0400

    remove too much top space

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,7 +18,6 @@ const Home = () => {
       <Stack
         align={'center'}
         spacing={{ base: 8, md: 10 }}
-        py={{ base: 20, md: 28 }}
         direction={{ base: 'column', md: 'row' }}
       >
         <Stack flex={1} spacing={{ base: 5, md: 10 }}>
