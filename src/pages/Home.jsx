b06653648c053369ba4f25f7896b9ea48d2a44f9
kizey <ikizey@icloud.com>
2023-01-03 20:17:08 +0400

    redo with Chakra

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,100 @@
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Heading,
+  Image,
+  Stack,
+  Text,
+} from '@chakra-ui/react';
+
 import { Link } from 'react-router-dom';
 import { ROUTE } from '../constants/Route';
 
 const Home = () => {
   return (
-    <div className='flex mx-5 flex-col md:flex-row md:mx-auto gap-2 md:mt-16'>
-      <img src='main.png' className='mx-auto rounded-xl mb-6 w-1/2' />
-      <div>
-        <h1 className='text-lg font-bold tracking-tight sm:text-3xl mb-6 mx-auto text-center'>
-          Experience the AI-powered education
-        </h1>
-
-        <p className='m-6 text-lg leading-8 text-gray-600 sm:text-center'>
-          We help K-12 studens to maximize their learning experience. Instantly generate
-          assignments and print them for self-study. Analyze and track student
-          progress. Share your ideas with other parents.
-        </p>
-        <div className='mt-8 flex gap-x-4 sm:justify-center'>
-          <Link
-            to={ROUTE.DEMO}
-            className='px-6 py-2 my-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900 mx-auto'
+    <Container maxW={'7xl'}>
+      <Stack
+        align={'center'}
+        spacing={{ base: 8, md: 10 }}
+        py={{ base: 20, md: 28 }}
+        direction={{ base: 'column', md: 'row' }}
+      >
+        <Stack flex={1} spacing={{ base: 5, md: 10 }}>
+          <Heading
+            lineHeight={1.1}
+            fontWeight={600}
+            fontSize={{ base: '3xl', sm: '4xl', lg: '6xl' }}
           >
-            Demo
-            <span className='ml-1 text-white' aria-hidden='true'>
-              &rarr;
-            </span>
-          </Link>
-        </div>
-      </div>
-    </div>
+            <Text
+              as={'span'}
+              position={'relative'}
+              _after={{
+                content: `""`,
+                width: 'full',
+                height: '30%',
+                position: 'absolute',
+                bottom: 1,
+                left: 0,
+                bg: 'red.400',
+                zIndex: -1,
+              }}
+            >
+              Experience
+            </Text>
+            <br />
+            <Text as={'span'} color={'red.400'}>
+              the AI-powered education
+            </Text>
+          </Heading>
+          <Text color={'gray.500'}>
+            We help K-12 students to maximize their learning experience.
+            Instantly generate assignments and print them for self-study.
+            Analyze and track student progress. Share your ideas with other
+            parents.
+          </Text>
+          <Button
+            as={Link}
+            to={ROUTE.DEMO}
+            rounded={'full'}
+            size={'lg'}
+            fontWeight={'normal'}
+            px={6}
+            colorScheme={'red'}
+            bg={'red.400'}
+            _hover={{ bg: 'red.500' }}
+          >
+            Demo &rarr;
+          </Button>
+        </Stack>
+        <Flex
+          flex={1}
+          justify={'center'}
+          align={'center'}
+          position={'relative'}
+          w={'full'}
+        >
+          <Box
+            position={'relative'}
+            // height={'300px'}
+            rounded={'2xl'}
+            boxShadow={'2xl'}
+            width={'full'}
+            overflow={'hidden'}
+          >
+            <Image
+              alt={'Hero Image'}
+              fit={'cover'}
+              align={'center'}
+              w={'100%'}
+              h={'100%'}
+              src={'main.png'}
+            />
+          </Box>
+        </Flex>
+      </Stack>
+    </Container>
   );
 };
 
