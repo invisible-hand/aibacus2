07138bd71c008dd6a19421895e554481e4c37181
kizey <ikizey@icloud.com>
2023-01-04 01:42:56 +0400

    redo with Chakra

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,14 +1,12 @@
+import { Image } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 import { ROUTE } from '../constants/Route';
 
 const Logo = () => {
   return (
-    <div className='flex lg:min-w-0 ' aria-label='Global'>
-      <Link to={ROUTE.INDEX} className='-m-1.5 p-1.5'>
-        <span className='sr-only'>Aibacus</span>
-        <img className='h-8' src='grid-outline.svg' alt='' />
-      </Link>
-    </div>
+    <Link to={ROUTE.INDEX}>
+      <Image h={8} src='grid-outline.svg' alt='logo of Aibacus' />
+    </Link>
   );
 };
 
