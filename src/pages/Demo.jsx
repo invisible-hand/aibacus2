4bb0cc32fc6cc0ba57e75c7d8bad4e21303f8f28
kizey <ikizey@icloud.com>
2023-01-03 22:28:56 +0400

    fix generate button

--- src/pages/Demo.jsx
+++ src/pages/Demo.jsx
@@ -1,4 +1,11 @@
-import { Container, SimpleGrid, Stack, Text, VStack } from '@chakra-ui/react';
+import {
+  Button,
+  Container,
+  SimpleGrid,
+  Stack,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
 import { GRADE, NUMBER_OF_TASKS } from '../api/promptChunks';
 
 import AssignmentHeading from '../components/AssignmentHeading';
@@ -80,13 +87,23 @@ const Demo = () => {
               disabled={true}
               after={' grade'}
             />
-            <button
-              className='block px-6 py-2 my-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900 disabled:bg-blue-200 hover:disabled:bg-blue-200'
-              disabled={isGenerating || ops.length === 0}
+
+            <Button
+              ml='1'
+              bg={'green.400'}
+              color={'white'}
+              // w='full'
+              size={'md'}
+              _hover={{
+                bg: 'green.500',
+              }}
+              isLoading={isGenerating}
+              loadingText='Generating...'
               onClick={responseHandler}
+              disabled={isGenerating || ops.length === 0}
             >
               {!isGenerating ? 'Generate' : 'Generating...'}
-            </button>
+            </Button>
           </VStack>
 
           <VStack align={'start'}>
