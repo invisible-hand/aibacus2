4f1cebdd9e7f6fbaf44a36a99330d4de893f0375
kizey <ikizey@icloud.com>
2023-01-04 03:50:32 +0400

    redo with Chakra

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -1,5 +1,14 @@
+import {
+  Box,
+  Button,
+  Heading,
+  Stack,
+  Text,
+  useColorModeValue,
+} from '@chakra-ui/react';
 import { Link, useNavigate } from 'react-router-dom';
 
+import Email from '../components/Email';
 import { ROUTE } from '../constants/Route';
 import { supabase } from '../supabaseClient';
 import { useState } from 'react';
@@ -20,7 +29,6 @@ const ResetPassword = () => {
 
       const { error } = await supabase.auth.resetPasswordForEmail(email, {
         redirectTo: `${window.location.protocol}://${window.location.host}${ROUTE.SET_NEW_PASSWORD}`,
-        // * for some reason incoming link doesn't have path to set-new-password path.
       });
       if (error) {
         throw new Error(error);
@@ -34,43 +42,48 @@ const ResetPassword = () => {
   };
 
   return (
-    <div className='flex items-center justify-center'>
-      <div className='px-8 py-6 mx-4 mt-4 text-left bg-white shadow-lg md:w-1/2 lg:w-1/2 sm:w-1/2'>
-        <h3 className='text-2xl font-bold text-center'>Password Recovery</h3>
-        <form onSubmit={handleSubmit}>
-          <div className='mt-4'>
-            <div className='mt-4'>
-              <label className='block' htmlFor='email'>
-                Email
-              </label>
-              <input
-                id='email'
-                type='text'
-                placeholder='Email'
-                className='w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600'
-              />
-            </div>
-            <div className='flex'>
-              <button
-                className='w-full px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900'
-                disabled={isFetching}
+    <Stack minW={460} spacing={8} mx={'auto'} maxW={'lg'} px={6} my={5}>
+      <Heading mx={'auto'} fontSize={'4xl'}>
+        Password Recovery
+      </Heading>
+      <form onSubmit={handleSubmit}>
+        <Box
+          rounded={'lg'}
+          bg={useColorModeValue('white', 'gray.700')}
+          boxShadow={'lg'}
+          p={8}
+        >
+          <Stack spacing={4}>
+            <Email />
+            <Stack spacing={10}>
+              <Button
+                bg={'green.400'}
+                color={'white'}
+                _hover={{
+                  bg: 'green.500',
+                }}
+                isDisabled={isFetching}
               >
                 Send recovery link
-              </button>
-            </div>
-            <div className='mt-2 text-grey-dark'>
-              Don't have an account?
-              <Link
-                to={ROUTE.REGISTER}
-                className='text-blue-600 hover:underline ml-2'
-              >
-                Register
-              </Link>
-            </div>
-          </div>
-        </form>
-      </div>
-    </div>
+              </Button>
+              <Box>
+                <Text as='span'>Don't have an account?</Text>
+                <Button
+                  ml={1}
+                  variant={'link'}
+                  display={'inline'}
+                  as={Link}
+                  to={ROUTE.REGISTER}
+                  color={'green.400'}
+                >
+                  Register
+                </Button>
+              </Box>
+            </Stack>
+          </Stack>
+        </Box>
+      </form>
+    </Stack>
   );
 };
 
