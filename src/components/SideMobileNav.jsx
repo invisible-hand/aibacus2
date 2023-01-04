af27c4a1e1ad278eae80158a7e9778de924f17bd
kizey <ikizey@icloud.com>
2023-01-04 17:17:06 +0400

    add SideMobileNav

---
+++ src/components/SideMobileNav.jsx
@@ -0,0 +1,26 @@
+import { Flex, IconButton, useColorModeValue } from '@chakra-ui/react';
+
+const SideMobileNav = ({ onOpen, ...rest }) => {
+  return (
+    <Flex
+      ml={{ base: 0, md: 60 }}
+      px={{ base: 4, md: 24 }}
+      height='20'
+      alignItems='center'
+      bg={useColorModeValue('white', 'gray.900')}
+      borderBottomWidth='1px'
+      borderBottomColor={useColorModeValue('gray.200', 'gray.700')}
+      justifyContent='flex-start'
+      {...rest}
+    >
+      <IconButton
+        variant='outline'
+        onClick={onOpen}
+        aria-label='open menu'
+        // icon={<FiMenu />}
+      />
+    </Flex>
+  );
+};
+
+export default SideMobileNav;
