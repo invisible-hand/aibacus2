bb095a2f4d6efebe9d007acf3db671eb17d0ebd7
kizey <ikizey@icloud.com>
2023-01-05 22:49:09 +0400

    use toast instead of alert

--- src/pages/Math.jsx
+++ src/pages/Math.jsx
@@ -1,4 +1,11 @@
-import { Container, SimpleGrid, Stack, Text, VStack } from '@chakra-ui/react';
+import {
+  Container,
+  SimpleGrid,
+  Stack,
+  Text,
+  VStack,
+  useToast,
+} from '@chakra-ui/react';
 import { GRADE, MATH, NUMBER_OF_TASKS } from '../utils/ai/promptChunks';
 import { useContext, useState } from 'react';
 
@@ -17,6 +24,7 @@ import { aiRequest } from '../utils/ai/aiRequest';
 import { saveAssignment } from '../utils/database/assignments';
 
 const Math = () => {
+  const toast = useToast();
   const { session } = useContext(AuthContext);
   const { childrenDB, hasChildren } = useContext(ChildrenContext);
 
@@ -48,7 +56,13 @@ const Math = () => {
         session.user.id
       );
     } catch (error) {
-      alert(error);
+      toast({
+        position: 'top',
+        title: 'Error',
+        description: `${error.description || error.message}`,
+        status: 'error',
+        isClosable: true,
+      });
     } finally {
       setIsGenerating(false);
     }
