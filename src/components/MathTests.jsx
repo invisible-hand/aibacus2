ff9aba6e68ab3f5874a7992d11ec967777ac167a
kizey <ikizey@icloud.com>
2023-01-07 02:50:55 +0400

    tune prompt, fix number of tasks

--- src/components/MathTests.jsx
+++ src/components/MathTests.jsx
@@ -19,10 +19,10 @@ import { aiRequestMathWordTasks } from '../utils/ai/aiRequest';
 import { useState } from 'react';
 
 const basePrompt =
-  'Generate %task_amount% math tests for %grade% grade in a form of a word task. With four variant of answers. 1 correct answer and 3 incorrect ones. ' +
+  'Generate %task_amount% math tests for %grade% grade in a form of a word problem. Each test should have four answers. ' +
   'Please, make sure it is advanced enough for %grade% grade. ' +
   'Reply in form of JSON with shape like this: ' +
-  '`{"grade": <grade>, "subject": "math", "type": "test", "tests": [{ "test":{"problem": "<problem_text>", "answerOptions":[{"value": "<value>", "units": "<units>"}, {"value": "<value>", "units": "<units>"}, {"value": "<value>", "units": "<units>"}, {"value": "<value>", "units": "<units>"}], "correctAnswerIndex": <index as integer 0 to 3>}},{ "test":...},...]}`. ' +
+  '`{"grade": "<grade>", "subject": "math", "type": "test", "tests": [{ "test": {"problem": "<problem_text>", "answerOptions": [{"value": "<value>", "units": "<units>"}, {"value": "<value>", "units": "<units>"}, {"value": "<value>", "units": "<units>"}, {"value": "<value>", "units": "<units>"}], "correctAnswerIndex": <index of correct answer>}, "solution": "<solution>"},{ "test":...},...]}`. ' +
   'It should be parsable with JSON.parse() without errors. ' +
   'Use minimal form of units for examples: "kg", "m³", "m/s²". ' +
   'Instead of "^0,^1,^2,^3,^4,^5,^6,^7,^8,^9,^0" etc. use ⁰,¹,²,³,⁴,⁵,⁶,⁷,⁸,⁹ symbols for exponents. Also use them in units. ' +
@@ -34,7 +34,7 @@ const MathTests = () => {
   const toast = useToast();
 
   const [grade, setGrade] = useState('3');
-  const [numberOfTasks, setNumberOfTasks] = useState('2');
+  const [numberOfTasks, setNumberOfTasks] = useState('3');
 
   const [oks, setOks] = useState(null);
   const [givenAnswers, setGivenAnswers] = useState(null);
@@ -106,7 +106,7 @@ const MathTests = () => {
         label={'Grade'}
       />
       <NumberOfTasks
-        defaultValue={'3'} //TODO!
+        defaultValue={numberOfTasks}
         onChange={setNumberOfTasks}
         label='Number of tests'
       />
