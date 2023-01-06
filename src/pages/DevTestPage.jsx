657488c0545df5b421c1c3fe98a59c249c817709
kizey <ikizey@icloud.com>
2023-01-06 23:46:24 +0400

    add reading tests to devpage

--- src/pages/DevTestPage.jsx
+++ src/pages/DevTestPage.jsx
@@ -2,11 +2,14 @@ import { Box, Stack } from '@chakra-ui/react';
 
 import MathTests from '../components/MathTests';
 import MathWordProblems from '../components/MathWordProblems';
+import ReadingTests from '../components/ReadingTests';
+
 export default () => {
   return (
     <Stack direction={'row'}>
       <MathWordProblems />
       <MathTests />
+      <ReadingTests />
     </Stack>
   );
 };
